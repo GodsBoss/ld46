91b31c50ad99de7323d470fdf07416a4fbefa384
Timo Reitz <godsboss@gmx.de>
2020-04-18 13:12:42 +0200

    Allow returning from high score list back to title (any key).

--- yic/hiscore.go
+++ yic/hiscore.go
@@ -14,6 +14,13 @@ func (h *hiscore) Tick(ms int) *engine.Transition {
 	return nil
 }
 
+func (h *hiscore) HandleKeyEvent(event engine.KeyEvent) *engine.Transition {
+	if event.Type != engine.KeyUp {
+		return nil
+	}
+	return engine.NewTransition(titleStateID)
+}
+
 func (h *hiscore) Objects() map[string][]engine.Object {
 	return map[string][]engine.Object{}
 }
