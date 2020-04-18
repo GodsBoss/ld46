0184f80063ea928895e2ebe43e4dd483d2cad4a8
Timo Reitz <godsboss@gmx.de>
2020-04-18 20:03:34 +0200

    Prepare for some more rendering.

--- main.go
+++ main.go
@@ -107,7 +107,7 @@ func run() error {
 				errsChan <- err
 				close(errsChan)
 			}
-			renderer := ui.NewRenderer(img, yic.Sprites(), []string{"background"})
+			renderer := ui.NewRenderer(img, yic.Sprites(), []string{"background", "fields", "entities", "ui"})
 			renderer.Zoom = 2
 			var reqAnimationFrameCallback func()
 			reqAnimationFrameCallback = func() {
