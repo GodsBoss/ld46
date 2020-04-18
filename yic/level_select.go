e5520aeff9503951c33c10482859ed5c452d076d
Timo Reitz <godsboss@gmx.de>
2020-04-18 13:45:11 +0200

    Use 'b' to return from level selection to title.

--- yic/level_select.go
+++ yic/level_select.go
@@ -14,6 +14,17 @@ func (ls *levelSelect) Tick(ms int) *engine.Transition {
 	return nil
 }
 
+func (ls *levelSelect) HandleKeyEvent(event engine.KeyEvent) *engine.Transition {
+	if event.Type != engine.KeyUp {
+		return nil
+	}
+	switch event.Key {
+	case "b":
+		return engine.NewTransition(titleStateID)
+	}
+	return nil
+}
+
 func (ls *levelSelect) Objects() map[string][]engine.Object {
 	return map[string][]engine.Object{}
 }
