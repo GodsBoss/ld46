55e41fbcd4c9dd3dd4e1676b0e96d856003be1b0
Timo Reitz <godsboss@gmx.de>
2020-04-19 23:13:30 +0200

    Extract pure level data into separate file.

---
+++ yic/level_data.go
@@ -0,0 +1,51 @@
+package yic
+
+var levelOne = `
+.>.v..XXXX
+.......OO.
+...>...OO.
+..........
+XXX..v..<.
+XXX.......
+...XX....X
+.........X
+.^...<...X
+X.......^.
+`
+
+var levelTwo = `
+>.v......v.<..
+....>.OO......
+......OO.<....
+..>.^......^.<
+`
+
+var levelThree = `
+.>XXXXv....XOO
+.>XXXXv.XXX.OO
+.>XXXXv.XXX.XX
+.>XXXXv.XXX.XX
+.....XX....XXX
+vXXXXX<>XXXX^X
+X.XXXXXX.....X
+X.XXXXXX.XXXXX
+X.....XX.....X
+XXX.XXXX.XXX.X
+XXX.XXXX.....X
+>XXXXXX^XXXXXX
+`
+
+var levelFour = `
+XvX..XX.....
+.....XX..XXX
+...v.XX...<.
+.....XX.....
+......v.<...
+XXXXXOOXXXXX
+XXXXXOOXXXXX
+...>..^.....
+.....XX.....
+.>...XX.^...
+XXX..XX.....
+.....XX..X^X
+`
